03ed673829c41a8b57bda44def90a9fa5507bc40
aramisjasso <aramisjasso@gmail.com>
2025-11-03 14:10:04 -0700

    chore: remove comments

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,24 +7,20 @@ sap.ui.define([
     return Controller.extend("com.cat.sapfioricatalogs.controller.App", {
         
         onInit: function () {
-            // (Paso 9 del tutorial)
             var oModel = new JSONModel();
-            // Usamos un path genérico para el modelo del menú
+
             oModel.loadData("./model/menuItems.json"); 
             this.getView().setModel(oModel, "sideMenuModel");
         },
 
-        // (Paso 5 del tutorial)
         onMenuButtonPress: function () {
             var oToolPage = this.byId("toolPage");
             oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
         },
 
-        // (Paso 6 del tutorial - Lógica de ruteo)
         onItemSelect: function (oEvent) {
             var sKey = oEvent.getParameter("item").getKey();
-            // Aquí tu compañero Sebas conectará el router
-            // Por ahora, solo navegamos al "key" (que debe coincidir con un "name" de tus rutas)
+            // Rífate, Sebas
             this.getOwnerComponent().getRouter().navTo(sKey);
         }
     });
