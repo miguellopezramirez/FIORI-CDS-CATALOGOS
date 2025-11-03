8ba4e8ca633ae373b63ecf9984bf0c979f187ca6
aramisjasso <aramisjasso@gmail.com>
2025-11-03 14:00:53 -0700

    refactor: replace entire project with a functional version

--- sap_fiori_catalogs/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-QUnit.config.autostart = false;
-
-sap.ui.require(["com/cat/sapfioricatalogs/test/integration/AllJourneys"
-], function () {
-	QUnit.start();
-});
--- sap_fiori_catalogs/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"com/cat/sapfioricatalogs/test/unit/controller/Main.controller"
-], function () {
-	"use strict";
-});
